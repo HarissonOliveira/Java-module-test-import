09e792fa9e1bb233509c81068a4c3efed4cc9726
Harisson Oliveira <harisson.batista.oliveira@everis.com>
2021-09-07 19:44:07 -0300

    First commit

---
+++ src/br/com/harisson/app/financeiro/Teste.java
@@ -0,0 +1,12 @@
+package br.com.harisson.app.financeiro;
+
+import br.com.harisson.app.calculo.Calculadora;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		System.out.println(calc.soma(2, 3, 4));
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module app.financeiro {
+	
+	requires java.base; // por padrão é sempre requerido
+	requires app.calculo;
+	
+}
